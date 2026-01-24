96719ed892de675d883f487812650d7bfd5fa21e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 22:55:23 +0900

    feat: implement complete sign-up form with input fields, submit button, and styling for the sign-up page.

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,27 +1,75 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const SignUp = () => {
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center p-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Sign Up</CardTitle>
-          <CardDescription>
+      <Card className="w-full max-w-md shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-primary">
+            Sign Up
+          </CardTitle>
+          <CardDescription className="text-muted-foreground">
             Create an account to start tracking your job applicatoins
           </CardDescription>
         </CardHeader>
-        <form>
-          <CardContent>
-            <div></div>
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" type="text" placeholder="John Doe" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="johndoe@example.com"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="John Doe"
+                required
+              />
+            </div>
           </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90"
+            >
+              Sign Up
+            </Button>
+            <p className="text-center text-sm text-muted-foreground">
+              Already have an account?
+              <Link
+                href={"/sign-in"}
+                className="font-medium text-primary hover:underline"
+              >
+                {" "}
+                Sign In
+              </Link>
+            </p>
+          </CardFooter>
         </form>
       </Card>
     </div>

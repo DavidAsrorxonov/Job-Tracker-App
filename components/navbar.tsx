9d7d38917d549bf5c6ae76dd9a9a720c5c8036e4
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 15:42:07 +0900

    feat: Add Navbar component using Radix UI Separator and integrate it into the layout.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,21 @@
+import { BriefcaseIcon } from "lucide-react";
+import Link from "next/link";
+import { Separator } from "./ui/separator";
+
 const Navbar = () => {
   return (
     <nav>
-      <div></div>
+      <div className="container mx-auto flex h-16 items-center px-4">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 text-xl font-bold text-primary"
+        >
+          <BriefcaseIcon />
+          Job Tracker
+        </Link>
+        <div></div>
+      </div>
+      <Separator />
     </nav>
   );
 };

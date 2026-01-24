cfe6a3e091a0903cdcdd3bd234fde8ad5c62b500
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 15:46:30 +0900

    feat: Add dark mode toggle to the navbar and adjust its layout for better spacing.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,12 @@ import { BriefcaseIcon } from "lucide-react";
 import Link from "next/link";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="container mx-auto flex h-16 items-center px-4">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
         <Link
           href={"/"}
           className="flex items-center gap-2 text-xl font-bold text-primary"
@@ -14,13 +15,14 @@ const Navbar = () => {
           <BriefcaseIcon />
           Job Tracker
         </Link>
-        <div>
+        <div className="flex items-center gap-4">
           <Link href={"/sign-in"}>
             <Button variant={"ghost"}>Log In</Button>
           </Link>
           <Link href={"/sign-up"}>
             <Button>Start for free</Button>
           </Link>
+          <ModeToggle />
         </div>
       </div>
       <Separator />

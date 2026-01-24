f609e4f2f3c768ae422b95fe20bf6812ea9e0894
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 22:11:11 +0900

    feat: implement initial sign-up page UI using Card components.

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,29 @@
 "use client";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>
+            Create an account to start tracking your job applicatoins
+          </CardDescription>
+        </CardHeader>
+        <form>
+          <CardContent></CardContent>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;

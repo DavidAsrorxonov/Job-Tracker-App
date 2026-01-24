347d65c6e800bd1792890d5c45b5fe189c220498
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 17:20:39 +0900

    refactor: Extract hero section from `app/page.tsx` into a new `Hero` component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,13 @@
 import Features from "@/components/Features";
+import Hero from "@/components/Hero";
 import ImageTabs from "@/components/image-tabs";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { ArrowRightIcon } from "lucide-react";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
-        <section className="container mx-auto px-4 py-32">
-          <div className="mx-auto max-w-4xl text-center">
-            <h1 className="mb-6 text-6xl font-bold">
-              A better way to track your job applications
-            </h1>
-            <p className="text-muted-foreground mb-10 text-xl">
-              Capture, organize and manage your job search in one place
-            </p>
-            <div className="flex flex-col items-center gap-4">
-              <Link href={"/sign-up"}>
-                <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                  Start for free
-                  <ArrowRightIcon className="ml-2" />
-                </Button>
-              </Link>
-              <p className="text-sm text-muted-foreground">
-                Free forever! No credit card required
-              </p>
-            </div>
-          </div>
-        </section>
+        <Hero />
 
         <Separator />
 

7253a1f31b1bcc1cbc97c3680a6fdd0f2fd25433
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 17:12:48 +0900

    refactor: Dynamically render image tabs by extracting content into new constants and types files.

--- components/image-tabs.tsx
+++ components/image-tabs.tsx
@@ -1,6 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import { Card } from "./ui/card";
 import Image from "next/image";
+import { tabsContent } from "@/constants/tabs-content";
 
 const ImageTabs = () => {
   return (
@@ -12,42 +13,18 @@ const ImageTabs = () => {
             <TabsTrigger value="img2">Get Hired</TabsTrigger>
             <TabsTrigger value="img3">Manage Boards</TabsTrigger>
           </TabsList>
-          <TabsContent value="img1">
-            <Card>
-              <div className="flex items-center justify-center h-full">
+          {tabsContent.map((tabContent) => (
+            <TabsContent value={tabContent.value} key={tabContent.value}>
+              <Card className="flex items-center justify-center h-full">
                 <Image
-                  src={"/hero-images/hero1.png"}
-                  alt="hero1"
+                  src={tabContent.image}
+                  alt={tabContent.alt}
                   width={1200}
                   height={800}
                 />
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent value="img2">
-            <Card>
-              <div className="flex items-center justify-center h-full">
-                <Image
-                  src={"/hero-images/hero2.png"}
-                  alt="hero2"
-                  width={1200}
-                  height={800}
-                />
-              </div>
-            </Card>
-          </TabsContent>
-          <TabsContent value="img3">
-            <Card>
-              <div className="flex items-center justify-center h-full">
-                <Image
-                  src={"/hero-images/hero3.png"}
-                  alt="hero3"
-                  width={1200}
-                  height={800}
-                />
-              </div>
-            </Card>
-          </TabsContent>
+              </Card>
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>

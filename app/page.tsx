6a933108879e075e2fde9d598ea667a059916f31
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-24 15:15:18 +0900

    feat: implement dark mode functionality and add foundational UI components like button and dropdown menu.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
@@ -5,7 +7,9 @@ export default function Home() {
         <section className="container mx-auto px-4 py-32">
           <div>
             <h1>A better way to track your job applications</h1>
+            <p>Capture, organize and manage your job search in one place!</p>
           </div>
+          <ModeToggle />
         </section>
       </main>
     </div>

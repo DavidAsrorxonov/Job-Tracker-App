4f719e6427daff3bbb4aee8bf30d464d89cfff98
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-26 17:13:19 +0900

    feat: Add default job application column definitions and their corresponding types.

---
+++ constants/default-columns.ts
@@ -0,0 +1,24 @@
+import { DefaultColumnsProps } from "@/types/default-columns";
+
+export const DEFAULT_COLUMNS: DefaultColumnsProps = [
+  {
+    name: "Wish List",
+    order: 0,
+  },
+  {
+    name: "Applied",
+    order: 1,
+  },
+  {
+    name: "Interviewing",
+    order: 2,
+  },
+  {
+    name: "Offer",
+    order: 3,
+  },
+  {
+    name: "Rejected",
+    order: 4,
+  },
+];

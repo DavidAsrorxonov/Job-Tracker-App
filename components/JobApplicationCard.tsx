4fae49487ee94643b596cfc983c588af5d7f7fe1
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-01-28 10:28:01 +0900

    feat: add job card dropdown scaffolding for edit and move actions

--- components/JobApplicationCard.tsx
+++ components/JobApplicationCard.tsx
@@ -1,7 +1,77 @@
 import { JobApplicationCardProps } from "@/interface/job-application-card";
+import { Card, CardContent } from "./ui/card";
+import { Edit2, ExternalLink, MoreVertical } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const JobApplicationCard = ({ job, columns }: JobApplicationCardProps) => {
-  return <div>JobApplicationCard</div>;
+  const { company, position, description, tags, notes, jobUrl } = job;
+
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <div>
+            <div>
+              <h3>{position}</h3>
+              <p>{company}</p>
+              {description && <p>{description}</p>}
+              {tags && tags.length > 0 && (
+                <div>
+                  {tags.map((tag, idx) => (
+                    <span key={idx}>{tag}</span>
+                  ))}
+                </div>
+              )}
+
+              {jobUrl && (
+                <a
+                  href={jobUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <ExternalLink />
+                </a>
+              )}
+            </div>
+
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant={"ghost"} size={"icon"}>
+                    <MoreVertical />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>
+                    <Edit2 />
+                    Edit
+                  </DropdownMenuItem>
+                  {columns.length > 1 && (
+                    <>
+                      {columns
+                        .filter((c) => c._id !== job.columnId)
+                        .map((col, idx) => (
+                          <DropdownMenuItem key={idx}>
+                            Move to {col.name}
+                          </DropdownMenuItem>
+                        ))}
+                    </>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default JobApplicationCard;

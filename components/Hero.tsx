9014985e7ac9dbdb26723f28bcf14f26fbd5b9aa
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-14 23:08:38 +0900

    feat(hero): add background gradient to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,6 +10,7 @@ import { Badge } from "./ui/badge";
 const Hero = () => {
   return (
     <section className="relative w-full py-32">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-size-[24px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <motion.div
         className="absolute inset-0 pointer-events-none -z-10"
         initial={{ opacity: 0 }}

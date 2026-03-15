7f8bc7a525ff04b1636fcacd4e013d3ac328e8e7
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-16 02:04:01 +0900

    feat(chart): add PieChartDonut component for activity distribution visualization

--- app/dashboard/[id]/timeline/_components/charts-wrapper.tsx
+++ app/dashboard/[id]/timeline/_components/charts-wrapper.tsx
@@ -1,10 +1,12 @@
 import { ITimelineEntry } from "@/lib/models/job-application";
 import { BarChartDefault } from "./bar-chart-default";
+import { PieChartDonut } from "./pie-chart-donut";
 
 const ChartWrapper = ({ timeline }: { timeline: ITimelineEntry[] }) => {
   return (
     <div className="w-full flex gap-3 items-center justify-between">
       <BarChartDefault timeline={timeline} />
+      <PieChartDonut timeline={timeline} />
     </div>
   );
 };

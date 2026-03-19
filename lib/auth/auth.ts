d7fae5ffbb70d9d3fe240ac0ea114d5803633284
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-19 22:33:55 +0900

    refactor(auth): remove unused email OTP and Resend imports

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -4,15 +4,9 @@ import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { initializeUserBoard } from "../init-user-board";
-import { emailOTP } from "better-auth/plugins";
-import { JobTrackerVerifyEmail } from "@/templates/job-tracker-email-verify-template";
-
-import { Resend } from "resend";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL_DEV!,

1d73253111894150987e6092285bf6241686c461
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-19 22:54:03 +0900

    feat(profile): implement ProfileSidebar component with active tab and tab change handler

--- app/profile/_components/profile-sidebar.tsx
+++ app/profile/_components/profile-sidebar.tsx
@@ -1,5 +1,16 @@
 "use client";
 
-// type Props = {
-//     activeTab: TabId
-// }
+import { useSession } from "@/lib/auth/auth-client";
+import { TabId } from "../page";
+
+type Props = {
+  activeTab: TabId;
+  onTabChange: (tab: TabId) => void;
+};
+
+const ProfileSidebar = ({ activeTab, onTabChange }: Props) => {
+  const { data: session } = useSession();
+  const user = session?.user;
+};
+
+export default ProfileSidebar;
